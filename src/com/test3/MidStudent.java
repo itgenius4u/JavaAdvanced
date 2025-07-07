542ce1c6644b3784c8d01bc3b7a773e380eb0629
HPE <HPE@HANLASAN-000>
2025-07-07 15:21:46 +0900

    ok

---
+++ src/com/test3/MidStudent.java
@@ -0,0 +1,7 @@
+package com.test3;
+
+public class MidStudent extends Student {
+	void gotoschool() {
+		System.out.println(this.getName() + "은 학교에 간다.");
+	}
+}
---
+++ src/com/test3/MyApp.java
@@ -0,0 +1,21 @@
+package com.test3;
+
+public class MyApp {
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		student1.setName("홍길동");
+		student1.setAge(20);
+		student1.studying();		
+		
+		Student student2 = new Student();
+		student2.setName("이순신");
+		student2.setAge(30);
+		student2.studying();
+		
+		MidStudent midstudent1 = new MidStudent();
+		midstudent1.setName("세종대왕");
+		midstudent1.setAge(40);
+		midstudent1.studying();
+		midstudent1.gotoschool();		
+	}
+}
