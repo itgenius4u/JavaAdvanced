7f701b67098199b00243bf245776094aff9286bb
HPE <HPE@HANLASAN-000>
2025-07-08 14:46:44 +0900

    ok

--- src/day02/com/test/MyClass5.java
+++ src/day02/com/test/MyClass5.java
@@ -1,10 +1,14 @@
 package day02.com.test;
 class Employee{
 	public int empNo; // 0
-	public Employee() {
+	public Employee() { 
 //		System.out.println("Default Employee Constructor");
 		this.empNo = 1000;
 	}
+	public Employee(int empNo) { // Overloading
+//		this();
+		this.empNo = empNo;	
+	}	
 	public int getEmpNo() { return this.empNo; }
 }
 public class MyClass5 {
@@ -16,5 +20,9 @@ public class MyClass5 {
 		Employee e = new Employee();
 		int ret = e.getEmpNo();
 		System.out.println(ret);
+		
+		Employee e2 = new Employee(1004);
+		int ret2 = e2.getEmpNo();
+		System.out.println(ret2);
 	}
 }
