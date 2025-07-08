1b5f50aae658e9e7d3be6b06bef574eb3722ec1f
HPE <HPE@HANLASAN-000>
2025-07-08 16:23:12 +0900

    ok

---
+++ src/day02/com/test2/MyClass4.java
@@ -0,0 +1,17 @@
+package day02.com.test2;
+
+import java.util.ArrayList;
+import day01.com.test3.Student;
+
+public class MyClass4 {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setName("이순신");
+		s.setAge(30);
+		System.out.println(s.getName());		
+		ArrayList al = new ArrayList();
+		al.add("1");
+		al.add(100);	
+	}
+}
