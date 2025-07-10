d149cf61ba596ddc44065d6b4cb9ce056f1383e1
HPE <HPE@HANLASAN-000>
2025-07-10 15:02:45 +0900

    ok

---
+++ src/day04/com/test3/MyClass3.java
@@ -0,0 +1,18 @@
+package day04.com.test3;
+class A extends Object {
+	private int aMember;
+	public void setAMember(int aMember) {
+		this.aMember = aMember;
+	}	
+	@Override
+	public String toString() {
+		return "aMember: " + this.aMember;
+	}
+}
+public class MyClass3 {
+	public static void main(String[] args) {
+		A a = new A();
+		a.setAMember(1000);
+		System.out.println(a.toString());
+	}
+}
