56e8b60bd29d056debadea83105bbe36ca7c1bc5
HPE <HPE@HANLASAN-000>
2025-07-10 16:13:26 +0900

    ok

---
+++ src/day04/com/test3/MyClass6.java
@@ -0,0 +1,13 @@
+package day04.com.test3;
+
+// 522, 523 예제.
+public class MyClass6 {
+	public static void main(String[] args) {
+		int a = 10;
+//		Integer i = new Integer(10);
+//		int c = i.intValue();
+		Integer i = a;
+		int c = i;
+		System.out.println(c	);
+	}
+}
