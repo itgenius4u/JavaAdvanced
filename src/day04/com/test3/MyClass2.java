72b0ef146d187ee7e1b6720e8268c07601044a75
HPE <HPE@HANLASAN-000>
2025-07-10 14:47:25 +0900

    ok

--- src/day04/com/test3/MyClass2.java
+++ src/day04/com/test3/MyClass2.java
@@ -12,16 +12,23 @@ package day04.com.test3;
 //	private Object m;
 //	public Object getM() { return this.m; }
 //}
-class MyData<T> {
+class MyData<T> { // class 기준으로 적용하는 방법
 	private T m;
 	public T getM() { return this.m; }
 }
+class MyMapData<K, V> {}
 public class MyClass2 {
+	// method 기준으로 적용하는 방법
+	static <T> T myValue(T e) {
+		return e;
+	}
+	// <? extends >, <? super >, <?>
 	public static void main(String[] args) {
 		MyData<Integer> m = new MyData<>();
 		m.getM();
 		MyData<String> m2 = new MyData<String>();
 		m2.getM();
-
+		int a = 10;
+		System.out.println(myValue(a));
 	}
 }
