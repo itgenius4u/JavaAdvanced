e7f895f381c70668c2565a6371e28771bf069bb1
HPE <HPE@HANLASAN-000>
2025-07-11 11:43:30 +0900

    ok

---
+++ src/day05/com/test/MyClass5.java
@@ -0,0 +1,43 @@
+package day05.com.test;
+class Employee {
+	private int empNo;
+	private String empName;
+	private int age;
+	private String address;
+	public Employee(int empNo, String empName, int age, String address) {
+		this.empNo = empNo;
+		this.empName = empName;
+		this.age = age;
+		this.address = address;
+	}
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
+public class MyClass5 {
+	public static void main(String[] args) {
+
+	}
+
+}
