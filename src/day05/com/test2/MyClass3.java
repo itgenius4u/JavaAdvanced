5d7aaaf8326ee2400ae45e104c0f9a9960d73ac2
HPE <HPE@HANLASAN-000>
2025-07-11 15:09:55 +0900

    ok

---
+++ src/day05/com/test2/MyClass3.java
@@ -0,0 +1,21 @@
+package day05.com.test2;
+class MyWork1 extends Thread {
+	public void run() {}
+}
+class MyWork2 extends Thread {
+	public void run() {}
+}
+class MyCount {	
+	private int count = 0;
+	public void incrementCount() {
+		count++;
+	}
+	public int getCount() {
+		return count;
+	}
+}
+public class MyClass3 {
+	public static void main(String[] args) {
+
+	}
+}
