0bd6a792591c2f9cdf11c08c87cd64432a1eb20f
HPE <HPE@HANLASAN-000>
2025-07-11 15:16:01 +0900

    ok

--- src/day05/com/test2/MyClass3.java
+++ src/day05/com/test2/MyClass3.java
@@ -14,7 +14,18 @@ class MyWork1 extends Thread {
 	}
 }
 class MyWork2 extends Thread {
-	public void run() {}
+	public void run() {
+		for(int inx = 0; inx < 10; inx++) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(this.getName() + ", " 
+					+ this.getId() + ", Count: "	);
+		}
+	}
 }
 class MyCount {	
 	private int count = 0;
@@ -27,6 +38,13 @@ class MyCount {
 }
 public class MyClass3 {
 	public static void main(String[] args) {
-
+		MyWork1 work1 = new MyWork1();
+		MyWork2 work2 = new MyWork2();
+		work1.start();
+		work2.start();
 	}
 }
+
+
+
+
