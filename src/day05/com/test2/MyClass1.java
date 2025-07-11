9947838e814881712d95918ad7e658cd01e0d62a
HPE <HPE@HANLASAN-000>
2025-07-11 14:30:51 +0900

    ok

--- src/day05/com/test2/MyClass1.java
+++ src/day05/com/test2/MyClass1.java
@@ -2,11 +2,19 @@ package day05.com.test2;
 
 class MyThread extends Thread {
 	private String threadName;
-	MyThread(String threadName) {
+	private int executeTime;
+	MyThread(String threadName, int executeTime) {
 		this.threadName = threadName;
+		this.executeTime = executeTime;
 	}
 	@Override
 	public void run() {
+		try {
+			Thread.sleep(executeTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println(threadName + ", " + this.getId());
 	}	
 }
@@ -17,5 +25,7 @@ public class MyClass1 {
 				mainThread.getId());
 		MyThread m1 = new MyThread("MyThread #1");
 		m1.start();
+		MyThread m2 = new MyThread("MyThread #2");
+		m2.start();
 	}
 }
